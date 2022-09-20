223e1fb6b8c601038b7410fe8e623c4c059cea2a
Tomi-422 <108313322+Tomi-422@users.noreply.github.com>
2022-09-20 02:03:16 -0300

    Actualizacion

---
+++ js/fin_compra.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Compra realizada con exito!',
+    text: 'Gracias por comprar con nosotros',
+    imageUrl: '../assets/img/gracias.png',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
